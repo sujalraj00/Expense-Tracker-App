f9cd4ee1982618d4d52833a1ee2542172808a01c
Sujal Raj <sujalraj611@gmail.com>
2024-04-05 14:28:00 +0530

    modalBottomSheet updated

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+import 'package:expense_tracker/models/expense.dart';
 
 class NewExpense extends StatefulWidget{
   const NewExpense({super.key});
@@ -12,7 +15,31 @@ class NewExpense extends StatefulWidget{
 class _NewExpenseState extends State<NewExpense>{
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  DateTime ? _selectedDate;
+  Category _selectedCategory = Category.leisure;
 
+void _presentDatePicker () async{
+  final now = DateTime.now();
+  final firstDate = DateTime(now.year-1, now.month, now.day);
+   final pickedDate = await showDatePicker(
+    context: context,
+    initialDate: now, 
+    firstDate: firstDate, 
+    lastDate: now
+    );
+  //print(pickedDate);               // this line will be executed once we get the value in pickedDate since await keyword tells flutter to wait until the value is available
+  setState(() {
+    _selectedDate =  pickedDate;
+  }); 
+
+}
+
+void _submitExpenseData(){
+  final enteredAmount =   double.tryParse(_amountController.text); //it converts string to double if it is able to convert or returns null
+  if(_titleController.text.trim().isEmpty){
+    // show error message 
+  }
+}
 
   @override
   void dispose() {
@@ -34,7 +61,10 @@ class _NewExpenseState extends State<NewExpense>{
               label: Text('title')
             ),
           ),
-          TextField(
+          Row(
+            children: [
+             Expanded(
+              child: TextField(
             controller: _amountController,
             keyboardType: TextInputType.number,
             decoration: const InputDecoration(
@@ -42,8 +72,41 @@ class _NewExpenseState extends State<NewExpense>{
               label: Text('amount'),
             ),
           ),
+          ),
+          SizedBox(width: 16,),
+          Expanded(
+            child : Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+               Text(_selectedDate == null?'No date selected' : formatter.format(_selectedDate!)),  // here we use ! to force dart to assume that this will not be null
+              IconButton(
+                onPressed: _presentDatePicker, 
+                icon: const Icon(Icons.calendar_month))
+            ],
+            )
+           
+          )
+            ],
+          ),
+          const SizedBox(height: 16,),
           Row(
             children: [
+              DropdownButton(
+                value: _selectedCategory,
+                items: Category.values.map((category) => DropdownMenuItem(
+                  value: category,
+                  child: Text(category.name.toUpperCase()))
+                 ).toList(),
+               onChanged: (value){
+                if(value == null){
+                  return;
+                }
+                setState(() {
+                  _selectedCategory = value;
+                });
+               }),
+              const Spacer(),
             TextButton(
               onPressed: (){
                 Navigator.pop(context); 
